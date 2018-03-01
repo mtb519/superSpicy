cfe01d7353766a8b1049582ed0ee0af630089458
Matthew Banning <mtb519@york.ac.uk>
2018-03-01 18:28:03 +0000

    Added map.py

---
+++ map.py
@@ -0,0 +1,11 @@
+class Map(object):
+	
+	def __init__(self, rows, columns, num_vehicles, num_rides, ride_bonus, num_steps):
+		super(Map, self).__init__()
+		self.rows = rows
+		self.columns = columns
+		self.num_vehicles = num_vehicles
+		self.num_rides = num_rides
+		self.ride_bonus = ride_bonus
+		self.num_steps = num_steps
+		
