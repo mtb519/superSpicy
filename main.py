ae9846e5b9769a839fa4b85e09c5a46df5ee9d20
Matthew Banning <mtb519@york.ac.uk>
2018-03-01 19:39:40 +0000

    Created classes

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+import map
+import ride
+
+Rides = []
+Map = None
+
 def read_input():
 	rows, columns, num_vehicles, num_rides, ride_bonus, num_steps = [int(s) for s in input().split(" ")]
+	Map = Map(rows, columns, num_vehicles, num_rides, ride_bonus, num_steps)
+	
 	for i in range(0, num_rides):
 		start_row, start_column, finish_row, finish_column, earliest_start, latest_finish = [int(s) for s in input().split(" ")]
-		
+		Rides.append(Ride((start_row, start_column), (finish_row, finish_column), earliest_start, latest_finish))
 
 def main():
 	read_input()
