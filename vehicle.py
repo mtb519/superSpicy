326b8ee8baf9903823eb45d96c9900212536e01f
Matthew Banning <mtb519@york.ac.uk>
2018-03-01 21:14:43 +0000

    Added score to vehicle

--- vehicle.py
+++ vehicle.py
@@ -4,4 +4,12 @@ class Vehicle:
         self.finishingCoords = (0, 0) #coords of the vehicle once it finishes its current ride
         self.finishingTime = 0 #step the car finishes its current ride
 
-
+    def score(self, Map, Ride):
+		distanceToStart = Map.distance(self.finishingCoords, Ride.startIntersect)
+		waitTime = Ride.earliest_start - (self.finishingTime + distanceToStart)
+		if self.finishingTime + distanceToStart + max(0, waitTime) + Ride.distance() > Ride.latestFinish:
+			return 0
+		else:
+			bonus = int(waitTime >= 0) * Map.ride_bonus
+			score = (Ride.distance() + bonus) / (distanceToStart + max(0, waitTime) + 1)
+			return score
