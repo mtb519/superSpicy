65de1447a2c21ed9e16cc955d6038e7fd977b60a
mrchazaaa2 <charlie.howlett@live.co.uk>
2018-03-01 20:46:08 +0000

    added vehicle

--- vehicle.py
+++ vehicle.py
@@ -1,4 +1,4 @@
-class vehicle:
+class Vehicle:
     def __init__(self):
         self.assigned = [] #all ride completed by this vehicle, last element is the current ride being executed
         self.finishingCoords = (0, 0) #coords of the vehicle once it finishes its current ride
