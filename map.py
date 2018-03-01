b5cc7dceeea25ab11fd2bcb5ff607083ad0b08b7
Matthew Banning <mtb519@york.ac.uk>
2018-03-01 20:12:19 +0000

    Added distance to map

--- map.py
+++ map.py
@@ -8,3 +8,6 @@ class Map(object):
 		self.num_rides = num_rides
 		self.ride_bonus = ride_bonus
 		self.num_steps = num_steps
+
+	def distance(self, pos1, pos2):
+		return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
